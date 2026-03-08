9c64f6007cb072361ae7a8e9b18805a9de5e0743
rubayetseason <rubayetislamseason5@gmail.com>
2026-03-08 22:39:37 +0600

    feat: updated readme file

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,3 +1,3 @@
-export const FRONTEND_URL = "http://localhost:3000" as const;
+export const FRONTEND_URL = "https://inkwell-nextjs-frontend.vercel.app" as const;
 export const OPENROUTER_API_BASE_URL = "https://openrouter.ai/api/v1" as const;
 export const OPENAI_MODEL = "openai/gpt-4o-mini" as const;
